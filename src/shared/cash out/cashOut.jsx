5497d572308470d68f73541d6d9f75384a3e66ca
ABid hasan <abeydhasan134@gmail.com>
2025-07-08 13:23:16 +0600

    cashout amount

--- src/shared/cash out/cashOut.jsx
+++ src/shared/cash out/cashOut.jsx
@@ -18,36 +18,42 @@ const CashOut = () => {
     formState: { errors },
   } = useForm();
   const [agentStatus, setAgentStatus] = useState(false);
-  const [btn1,setBtn1]=useState(false);
-  const [agentNumber,setAgentNumber]=useState()
+  const [btn1, setBtn1] = useState(false);
+  const [agentNumber, setAgentNumber] = useState();
+  const [cashStatus, setCashStatus] = useState(false);
+  const [btn2, setBtn2] = useState(false);
+  const [cashAmout, setCashAmout] = useState();
   // this will log the value on every change
-   const handleAgentNumberChange = (e) => {
+  const handleAgentNumberChange = (e) => {
     const value = e.target.value;
 
     if (value.length === 11) {
       console.log("valid number", value);
       setAgentStatus(true);
-      setAgentNumber(value)
+      setAgentNumber(value);
     } else {
       console.log("agent number is not valid", value);
       setAgentStatus(false);
     }
   };
-  const onSubmit = (data) => {
-    console.log("Cash Out Submitted Data:", data);
-    // send data to backend here
-    // const res=axiosPublic.patch(`/cashOut`);
-    // console.log(res.data);
+  const handleAmoutChange = (e) => {
+    const value = e.target.value;
+
+   
+      setCashStatus(true);
+      setCashAmout(value);
+   
+      console.log("your amount of cash out is", value);
+      // setAgentStatus(false);
+   
   };
   const handelNumAgent = () => {
-    // const value = e.target.value;
-    // if (value.length === 11) {
-      setBtn1(true);
-      // setAgentNumber(value)
-      console.log("agent number is", agentNumber);
-    // } else {
-    //   console.log("agent number is not valid");
-    // }
+    setBtn1(true);
+    console.log("agent number is", agentNumber);
+  };
+  const handelCashAmount = () => {
+    setBtn2(true);
+    console.log("cash out amount on submit", cashAmout);
   };
   return (
     <motion.div
@@ -61,58 +67,80 @@ const CashOut = () => {
         <h2 className="text-2xl font-bold mb-4">Cash Out</h2>
       </div>
 
-      <div className="relative w-full pt-5 flex justify-center items-center text-center bg-black border-2 border-red-500 py-6">
-        {/* <form action="" onSubmit={handleSubmit(onSubmit)}> */}
+      <div className="relative w-full pt-5 flex justify-center items-center text-center bg-black py-6">
+        {/* agent number */}
+        <>
         <input
           type="text"
           placeholder="Enter your agent number"
           {...register("agentNumber", { required: true })}
           onChange={handleAgentNumberChange}
-          className={`${btn1 === true ? "relative -left-96 hidden" : "my-5 peer w-[230px] border-0 border-b-2 border-gray-400 bg-transparent py-2.5 text-white text-[17px] placeholder-transparent focus:border-b-[3px] focus:outline-none focus:ring-0 focus:border-b-gradient-to-r focus:from-[#116399] focus:to-[#38caef] font-normal focus:font-bold transition-all duration-200"}`}
+          className={`${btn1 === true ? "hidden" : "my-5 peer w-[230px] border-0 border-b-2 border-gray-400 bg-transparent py-2.5 text-white text-[17px] placeholder-transparent focus:border-b-[3px] focus:outline-none focus:ring-0 focus:border-b-gradient-to-r focus:from-[#992311] focus:to-[#4476d2] font-normal focus:font-bold transition-all duration-200"}`}
         />
 
         <label
           htmlFor="agentNumber"
-          className={`${btn1 === true ? "absolute -left-96" : "absolute left-40 top-0 text-[17px] text-gray-400 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-[17px] peer-placeholder-shown:cursor-text peer-focus:top-0 peer-focus:text-[17px] peer-focus:text-[#38caef] peer-focus:font-bold"}`}
+          className={`${btn1 === true ? "absolute -left-96 hidden" : "absolute left-40 top-0 text-[17px] text-gray-400 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-[17px] peer-placeholder-shown:cursor-text peer-focus:top-0 peer-focus:text-[17px] peer-focus:text-[#4476d2] peer-focus:font-bold"}`}
         >
           Agent Number
         </label>
-
-        {/* <input
-          type="text"
-          placeholder="Enter your cash out amount"
-          {...register("cashAmount", { required: true })}
-          // onChange={handleAgentNumberChange}
-          className={`${agentStatus === true ? "my-5 peer w-[230px] border-0 border-b-2 border-red-400 bg-transparent py-2.5 text-white text-[17px] placeholder-transparent focus:border-b-[3px] focus:outline-none focus:ring-0 focus:border-b-gradient-to-r focus:from-[#116399] focus:to-[#38caef] font-normal focus:font-bold transition-all duration-200" : "relative -left-96 hidden"}`}
-        />
-
-        <label
-          htmlFor="agentNumber"
-          className={`${agentStatus === true ? "absolute -left-96" : "absolute left-40 top-0 text-[17px] text-gray-400 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-[17px] peer-placeholder-shown:cursor-text peer-focus:top-0 peer-focus:text-[17px] peer-focus:text-[#38caef] peer-focus:font-bold"}`}
-        >
-          Agent Number
-        </label> */}
-
-        {
-          //   agentNum===true?<button type="submit">
-          //   <FaArrowCircleRight className="text-4xl scale-90 hover:scale-100 duration-[1s] hover:text-[#FF0072]" />
-          // </button>:
-          <button type="submit" onClick={handleSubmit(handelNumAgent)}>
-            <FaArrowCircleRight
-              className={`${agentStatus === true ? "text-4xl scale-90 hover:scale-100 duration-[1s] hover:text-[#FF0072]" : "text-4xl text-gray-900"}`}
-            />
-          </button>
-        }
-
-        {/* </form> */}
-      </div>
-
-      {errors.agentNumber && (
-        <p className="text-red-500 text-sm text-center -mt-3">
+        {errors.agentNumber && (
+        <p className="text-red-500 text-sm text-center -top-10 z-10">
           Agent number is required
         </p>
       )}
 
+        {btn1 === true ? (
+          ""
+        ) : (
+          <button type="submit" onClick={handleSubmit(handelNumAgent)}>
+            <FaArrowCircleRight
+              className={`${agentStatus === true ? `text-4xl scale-90 hover:scale-100 duration-[1s] hover:text-[#4476d2] ` : "text-4xl text-gray-900"}?`}
+            />
+          </button>
+        )}
+        </>
+        {/* cash out amount */}
+      
+      {
+        btn1===true?
+        <>
+        <input
+          type="text"
+          placeholder="Enter your agent number"
+          {...register("cashAmount", { required: true,max: 30000,min: 10 })}
+          onChange={handleAmoutChange}
+          className={`${btn2 === true ? "relative -left-96 hidden" : "my-5 peer w-[230px] border-0 border-b-2 border-gray-400 bg-transparent py-2.5 text-white text-[17px] placeholder-transparent focus:border-b-[3px] focus:outline-none focus:ring-0 focus:border-b-gradient-to-r focus:from-[#116399] focus:to-[#38caef] font-normal focus:font-bold transition-all duration-200"}`}
+        />
+
+        <label
+          htmlFor="cashAmount"
+          className={`${btn2 === true ? "absolute -left-96 hidden" : "absolute left-40 top-0 text-[17px] text-gray-400 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-[17px] peer-placeholder-shown:cursor-text peer-focus:top-0 peer-focus:text-[17px] peer-focus:text-[#38caef] peer-focus:font-bold"}`}
+        >
+          Enter your cash out amount 
+        </label>
+          
+          {
+            btn2===true?"":
+            <button type="submit" onClick={handleSubmit(handelCashAmount)}>
+            <FaArrowCircleRight
+              className={`${cashStatus === true ? `text-4xl scale-90 hover:scale-100 duration-[1s] hover:text-[#FF0072] ` : "text-4xl text-gray-900"}?`}
+            />
+          </button>
+          }
+        </>:""
+      } 
+      {errors.cashAmount && (
+        <p className="text-red-500 text-sm text-center -top-10 z-10">
+          {errors.cashAmount.message}
+        </p>
+      )}
+      
+      </div>
+
+      
+      
+      {/* QR code sceaner */}
       <div className="grid justify-center items-center">
         <button
           type="button"
